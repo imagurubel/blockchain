9857b8536da137b84e853d4060963576628e2f95
panda <panda>
2018-10-06 21:22:47 +0300

    All contract functions

--- contracts/Panda.sol
+++ contracts/Panda.sol
@@ -5,11 +5,15 @@ contract Panda {
 	uint constant statusCreated = 1;
 	uint constant statusConfirmed = 2;
 	uint constant statusResultSubmitted = 3;
-	uint constant statusCompleted = 4;
-	uint constant statusReviewStarted = 5;
-	uint constant statusReviewSubmited = 6;
-	uint constant reviewerVerdictGood = 1;
-	uint constant reviewerVerdictBaad = 2;
+	uint constant statusCompletedSuccessfully = 4;
+	uint constant statusCompletedUnsuccessfully = 5;
+	uint constant statusReviewStarted = 6;
+	uint constant statusReviewSubmited = 7;
+	uint constant statusArbitrageRequested = 8;
+	uint constant statusArbitrageCompleted = 9;
+	uint constant statusCanceledBeforeConfirmed = 10;
+	uint constant verdictGood = 1;
+	uint constant verdictBaad = 2;
 	uint minServiceFee = 10000000000000000; // 0.01 ETH
 	uint minPerformerRatingWithoutReview = 8;
 
@@ -22,21 +26,25 @@ contract Panda {
 		address reviewer;
 		bytes32 taskHash;
 		bytes32 resultHash;
-		uint creatorRating;
+		uint creatorRatingByPerformer;
+		uint creatorRatingByReviewer;
 		uint performerRating;
-		uint revieverRating;
+		uint reviewerRating;
 		uint creatorPays;
 		uint performerPays;
 		uint reviewerVerdict;
+		uint arbitrageVerdict;
 	}
 
 	struct TaskTiming {
+		uint duration;
 		uint createdAt;
 		uint confirmedAt;
 		uint resultSubmittedAt;
 		uint completedAt;
 		uint reviewStartedAt;
 		uint reviewSubmittedAt;
+		uint arbitrageSubmittedAt;
 	}
 
 	mapping (uint256 => Task) public tasks;
@@ -45,7 +53,7 @@ contract Panda {
 
 
 
-	function taskCreate(bytes32 _taskHash, address _performer) public payable returns (uint256) {
+	function taskCreate(bytes32 _taskHash, address _performer, uint _duration) public payable returns (uint256) {
 		require(msg.value > minServiceFee);
 
 		lastTaskId = lastTaskId + 1;
@@ -56,8 +64,19 @@ contract Panda {
 		tasks[lastTaskId].creatorPays = msg.value;
 		tasks[lastTaskId].performerPays = msg.value * 20 / 100;
 		tasksTiming[lastTaskId].createdAt = block.timestamp;
+		tasksTiming[lastTaskId].duration =  _duration;
 
 		return lastTaskId;
+	}
+
+
+	function taskCancelBeforeConfirmed(uint _taskId) public {
+		require(tasks[_taskId].creator == msg.sender);
+		require(tasks[_taskId].status == statusCreated);
+
+		tasks[_taskId].status = statusCanceledBeforeConfirmed;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		tasks[_taskId].creator.transfer(tasks[_taskId].creatorPays);
 	}
 
 
@@ -71,10 +90,23 @@ contract Panda {
 	}
 
 
+	function taskCompleteUnsuccessfullyWithoutResultSubmittedAfterTimeout(uint _taskId, uint _performerRating) public {
+		require(tasks[_taskId].creator == msg.sender);
+		require(tasks[_taskId].status == statusConfirmed);
+		require(block.timestamp > tasksTiming[_taskId].createdAt + tasksTiming[_taskId].duration);
+		require(_performerRating <= 10);
+
+		tasks[_taskId].performerRating = _performerRating;
+		tasks[_taskId].status = statusCompletedUnsuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		tasks[_taskId].creator.transfer(tasks[_taskId].creatorPays);
+		tasks[_taskId].performer.transfer(tasks[_taskId].performerPays);
+	}
+
+
 	function taskSubmitResult(uint _taskId, bytes32 _resultHash) public {
 		require(tasks[_taskId].performer == msg.sender);
-		require(tasks[_taskId].status == statusConfirmed
-			|| tasks[_taskId].status == statusResultSubmitted);
+		require(tasks[_taskId].status == statusConfirmed);
 
 		tasks[_taskId].status = statusResultSubmitted;
 		tasks[_taskId].resultHash = _resultHash;
@@ -88,7 +120,7 @@ contract Panda {
 		require(_performerRating >= minPerformerRatingWithoutReview);
 		require(_performerRating <= 10);
 
-		tasks[_taskId].status = statusCompleted;
+		tasks[_taskId].status = statusCompletedSuccessfully;
 		tasksTiming[_taskId].completedAt = block.timestamp;
 		tasks[_taskId].performerRating = _performerRating;
 		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, 0);
@@ -97,9 +129,25 @@ contract Panda {
 	}
 
 
+	function taskCompleteSuccessfullyByPerformerAfterTimeout(uint _taskId) public {
+		require(tasks[_taskId].performer == msg.sender);
+		require(
+			(tasks[_taskId].status == statusResultSubmitted && block.timestamp > tasksTiming[_taskId].resultSubmittedAt + 90 days)
+			|| (tasks[_taskId].status == statusReviewStarted && block.timestamp > tasksTiming[_taskId].reviewStartedAt + 90 days)
+			);
+
+		tasks[_taskId].status = statusCompletedSuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, 0);
+		tasks[_taskId].performer.transfer(peroformerFee);
+		tasks[_taskId].creator.transfer(calculateCreatorMoneyback(tasks[_taskId].creatorPays, peroformerFee, 0));
+	}
+
+
 	function taskReviewStart(uint _taskId, address _reviewer) public {
 		require(tasks[_taskId].creator == msg.sender);
-		require(tasks[_taskId].status == statusResultSubmitted);
+		require(tasks[_taskId].status == statusResultSubmitted
+			|| (tasks[_taskId].status == statusReviewStarted && block.timestamp > tasksTiming[_taskId].reviewStartedAt + 7 days));
 
 		tasks[_taskId].status = statusReviewStarted;
 		tasks[_taskId].reviewer = _reviewer;
@@ -110,7 +158,8 @@ contract Panda {
 	function taskReviewSubmit(uint _taskId, uint _reviewerVerdict, uint _performerRating) public {
 		require(tasks[_taskId].reviewer == msg.sender);
 		require(tasks[_taskId].status == statusReviewStarted);
-		require(_reviewerVerdict == reviewerVerdictGood || _reviewerVerdict == reviewerVerdictBaad);
+		require(_reviewerVerdict == verdictGood || _reviewerVerdict == verdictBaad);
+		require(_performerRating <= 10);
 
 		tasks[_taskId].reviewerVerdict = _reviewerVerdict;
 		tasks[_taskId].status = statusReviewSubmited;
@@ -119,13 +168,14 @@ contract Panda {
 	}
 
 
-	function taskCompleteSuccessfullyAfterReview(uint _taskId, uint _revieverRating) public {
+	function taskCompleteSuccessfullyAfterReview(uint _taskId, uint _reviewerRating) public {
 		require(tasks[_taskId].creator == msg.sender);
 		require(tasks[_taskId].status == statusReviewSubmited);
+		require(_reviewerRating <= 10);
 
-		tasks[_taskId].status = statusCompleted;
+		tasks[_taskId].status = statusCompletedSuccessfully;
 		tasksTiming[_taskId].completedAt = block.timestamp;
-		tasks[_taskId].revieverRating = _revieverRating;
+		tasks[_taskId].reviewerRating = _reviewerRating;
 		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
 		tasks[_taskId].reviewer.transfer(reviewerFee);
 		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, reviewerFee);
@@ -133,16 +183,118 @@ contract Panda {
 	}
 
 
-	// TODO taskComplete with moneyback with performer agreement or service approval
-
-
-	function taskRateCreator(uint _taskId, uint _creatorRating) public {
+	function taskCompleteSuccessfullyAfterReviewByPerformerAfterTimeout(uint _taskId) public {
 		require(tasks[_taskId].performer == msg.sender);
-		require(tasks[_taskId].status == statusCompleted);
-		require(_creatorRating <= 10);
-		require(tasks[_taskId].creatorRating == 0);
+		require(tasks[_taskId].status == statusReviewSubmited);
+		require(block.timestamp > tasksTiming[_taskId].reviewSubmittedAt + 7 days);
 
-		tasks[_taskId].creatorRating = _creatorRating;
+		tasks[_taskId].status = statusCompletedSuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
+		tasks[_taskId].reviewer.transfer(reviewerFee);
+		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, reviewerFee);
+		tasks[_taskId].performer.transfer(peroformerFee);
+	}
+
+
+	function taskCompleteUnsuccessfullyAfterReviewWithoutArbitrageAfterTimeout(uint _taskId, uint _reviewerRating) public {
+		require(tasks[_taskId].creator == msg.sender);
+		require(tasks[_taskId].status == statusReviewSubmited);
+		require(tasks[_taskId].reviewerVerdict == verdictBaad);
+		require(block.timestamp > tasksTiming[_taskId].reviewSubmittedAt + 7 days);
+		require(_reviewerRating <= 10);
+
+		tasks[_taskId].status = statusCompletedUnsuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		tasks[_taskId].reviewerRating = _reviewerRating;
+		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
+		tasks[_taskId].reviewer.transfer(reviewerFee);
+		tasks[_taskId].creator.transfer(calculateCreatorMoneyback(tasks[_taskId].creatorPays, 0, reviewerFee));
+	}
+
+
+	function taskRequestArbitrage(uint _taskId) public {
+		require(tasks[_taskId].creator == msg.sender || tasks[_taskId].performer == msg.sender);
+		require(tasks[_taskId].status == statusReviewSubmited);
+
+		tasks[_taskId].status = statusArbitrageRequested;
+	}
+
+
+	function taskArbitrageVerdict(uint _taskId, uint _arbitrageVerdict) public {
+		require(contractOwner == msg.sender);
+		require(_arbitrageVerdict == verdictGood || _arbitrageVerdict == verdictBaad);
+		require(tasks[_taskId].status == statusArbitrageRequested);
+
+		tasks[_taskId].status = statusArbitrageCompleted;
+		tasks[_taskId].arbitrageVerdict = _arbitrageVerdict;
+		tasksTiming[_taskId].arbitrageSubmittedAt = block.timestamp;
+	}
+
+
+	function taskCompleteUnsuccessfullyAfterArbitrage(uint _taskId, uint _reviewerRating) public {
+		require(tasks[_taskId].creator == msg.sender);
+		require(tasks[_taskId].status == statusArbitrageCompleted);
+		require(tasks[_taskId].arbitrageVerdict == verdictBaad);
+		require(_reviewerRating <= 10);
+
+		tasks[_taskId].status = statusCompletedUnsuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		tasks[_taskId].reviewerRating = _reviewerRating;
+		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
+		tasks[_taskId].reviewer.transfer(reviewerFee);
+		tasks[_taskId].creator.transfer(calculateCreatorMoneyback(tasks[_taskId].creatorPays, 0, reviewerFee));
+	}
+
+
+	function taskCompleteSuccessfullyAfterArbitrageByCreator(uint _taskId, uint _reviewerRating) public {
+		require(tasks[_taskId].creator == msg.sender);
+		require(tasks[_taskId].status == statusArbitrageCompleted);
+		require(tasks[_taskId].arbitrageVerdict == verdictGood);
+		require(_reviewerRating <= 10);
+
+		tasks[_taskId].status = statusCompletedSuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		tasks[_taskId].reviewerRating = _reviewerRating;
+		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
+		tasks[_taskId].reviewer.transfer(reviewerFee);
+		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, reviewerFee);
+		tasks[_taskId].performer.transfer(peroformerFee);
+	}
+
+
+	function taskCompleteSuccessfullyAfterArbitrageByPerformerAfterTimeout(uint _taskId) public {
+		require(tasks[_taskId].performer == msg.sender);
+		require(tasks[_taskId].status == statusArbitrageCompleted);
+		require(tasks[_taskId].arbitrageVerdict == verdictGood);
+		require(block.timestamp > tasksTiming[_taskId].arbitrageSubmittedAt + 7 days);
+
+		tasks[_taskId].status = statusCompletedSuccessfully;
+		tasksTiming[_taskId].completedAt = block.timestamp;
+		uint reviewerFee = calculateReviewerFee(tasks[_taskId].creatorPays);
+		tasks[_taskId].reviewer.transfer(reviewerFee);
+		uint peroformerFee = calculatePeroformerFee(tasks[_taskId].creatorPays, tasks[_taskId].performerPays, reviewerFee);
+		tasks[_taskId].performer.transfer(peroformerFee);
+	}
+
+
+	function taskRateCreatorByPerformer(uint _taskId, uint _creatorRatingByPerformer) public {
+		require(tasks[_taskId].performer == msg.sender);
+		require(tasks[_taskId].status == statusCompletedSuccessfully || tasks[_taskId].status == statusCompletedUnsuccessfully);
+		require(_creatorRatingByPerformer <= 10);
+		require(tasks[_taskId].creatorRatingByPerformer == 0);
+
+		tasks[_taskId].creatorRatingByPerformer = _creatorRatingByPerformer;
+	}
+
+
+	function taskRateCreatorByReviewer(uint _taskId, uint _creatorRatingByReviewer) public {
+		require(tasks[_taskId].reviewer == msg.sender);
+		require(tasks[_taskId].status == statusCompletedSuccessfully || tasks[_taskId].status == statusCompletedUnsuccessfully);
+		require(_creatorRatingByReviewer <= 10);
+		require(tasks[_taskId].creatorRatingByReviewer == 0);
+
+		tasks[_taskId].creatorRatingByReviewer = _creatorRatingByReviewer;
 	}
 
 	function calculateReviewerFee(uint _creatorPays) pure public returns (uint256) {
@@ -156,7 +308,6 @@ contract Panda {
 	function calculateCreatorMoneyback(uint _creatorPays, uint _performerFee, uint _reviewerFee) view public returns (uint256) {
 		return _creatorPays - _reviewerFee - _performerFee - minServiceFee;
 	}
-
 
 
 	constructor() public {
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Panda = artifacts.require("./Panda.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+	deployer.deploy(Migrations);
+	deployer.deploy(Panda);
 };
